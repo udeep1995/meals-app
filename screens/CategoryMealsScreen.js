40b2e42d1e856c2f9fe960364294d8de4463aa10
udeepDev <udeep.garg@forestlaketech.com>
2020-05-30 11:36:38 +0530

    [Added] all screens to be used in App Initial commit

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const CategoryMealsScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Categories Meals Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default CategoryMealsScreen;
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+
+const MealDetailScreen = props => {   return (
+    <View style = {styles.screen}>
+        <Text>The Meals Details Screen</Text>
+    </View>
+)
+};
+
+const styles = StyleSheet.create({
+screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+}
+})
+export default MealDetailScreen;
